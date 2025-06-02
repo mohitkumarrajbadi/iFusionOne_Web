6b6b4758b55b852ab6615b46f02ee9de0b3181a2
Siddharth Kumar <40791172+ryukirisame@users.noreply.github.com>
2025-06-02 13:16:38 +0530

    integrate jsonviewer with ifusion

--- ifusionone-frontend/src/features/json-schema-viewer/JsonViewer.tsx
+++ ifusionone-frontend/src/features/json-schema-viewer/JsonViewer.tsx
@@ -22,8 +22,47 @@ const nodeTypes: NodeTypes = {
   jsonNode: JsonNode,
 };
 
-function JsonViewer({ json }: { json: string }) {
-  const g = React.useMemo(() => useJsonGraph(json), [json]);
+const sampleJson = `
+  {
+	"items":
+		{
+			"item":
+				[
+					{
+						"id": "0001",
+						"type": "donut",
+						"name": "Cake",
+						"ppu": 0.55,
+						"batters":
+							{
+								"batter":
+									[
+										{ "id": "1001", "type": "Regular" },
+										{ "id": "1002", "type": "Chocolate" },
+										{ "id": "1003", "type": "Blueberry" },
+										{ "id": "1004", "type": "Devil's Food" }
+									]
+							},
+						"topping":
+							[
+								{ "id": "5001", "type": "None" },
+								{ "id": "5002", "type": "Glazed" },
+								{ "id": "5005", "type": "Sugar" },
+								{ "id": "5007", "type": "Powdered Sugar" },
+								{ "id": "5006", "type": "Chocolate with Sprinklesssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" },
+								{ "id": "5003", "type": "Chocolate" },
+								{ "id": "5004", "type": "Maple" }
+							]
+					}
+
+				]
+		}
+}
+  `;
+
+
+function JsonViewer() {
+  const g = React.useMemo(() => useJsonGraph(sampleJson), [sampleJson]);
 
   const [collapsed, setCollapsed] = useState<Set<string>>(new Set());
 
